ffc15005c7924052427f4be58fe1899c404df075
NikKonshin <konbshin1996@gmail.c0m>
2020-08-02 17:54:25 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/ArrayProcessing.java
@@ -0,0 +1,59 @@
+package com.geekbrains;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayProcessing {
+
+//    public static void main(String[] args) {
+//        int[] arr = new int[]{4,1,1,1,1};
+//       // System.out.println("main " + Arrays.toString(newArray(arr)));
+//        System.out.println("main1 " + checkArray(arr));
+//    }
+
+
+    public static int[] newArray(int arr[]) throws RuntimeException {
+    int countNumber = 0;
+    int serialNumber = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == 4){
+                countNumber++;
+                serialNumber = i;
+            }
+        }
+
+        int[] newArray = new int[arr.length - serialNumber-1];
+
+        if (countNumber!=0){
+            int j = 0;
+            for (int i = 0; i <arr.length ; i++) {
+                if(arr[i] != 4 && i > serialNumber) {
+                    newArray[j] = arr[i];
+                    j++;
+                }
+            }
+        } else {
+            throw new RuntimeException();
+        }
+
+    return newArray;
+    }
+
+    public static boolean checkArray(int arr[]) {
+        int countFor = 0;
+        int countOne = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == 4){
+                countFor++;
+            }
+            if (arr[i] == 1){
+                countOne++;
+            }
+        }
+        if(countFor != 0 && countOne != 0 && countFor + countOne == arr.length){
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/TestArrayProcessing.java
@@ -0,0 +1,10 @@
+import com.geekbrains.ArrayProcessing;
+import org.junit.Test;
+
+public class TestArrayProcessing {
+@Test
+    public  void testNewArray(){
+    ArrayProcessing arrayProcessing = new ArrayProcessing();
+    
+}
+}
